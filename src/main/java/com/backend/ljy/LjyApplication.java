1e7fc1af1b9ba7016e596d5531fdd7f8758838aa
wpdud1221@naver.com <wpdud1221@naver.com>
2025-02-10 23:52:18 +0900

    프로젝트 초기세팅

---
+++ src/main/java/com/backend/ljy/LjyApplication.java
@@ -0,0 +1,13 @@
+package com.backend.ljy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LjyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LjyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/backend/ljy/LjyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend.ljy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LjyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
